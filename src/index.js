0530ca9c7db9d00fb033e6ec9751363a2ef1c81d
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-12-24 15:48:15 +0200

    Fixed a warning related to theme usage of the Typography component.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Fix the warning for Typography components.
+// @see https://material-ui.com/style/typography/#migration-to-typography-v2
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
