ce741c859974ec8c9b7f16ea0c20493b0efb9469
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-02-16 22:52:09 +0200

    Better UI for the player dead component.

--- src/public/story-read/view/components/ending/PlayerDead.js
+++ src/public/story-read/view/components/ending/PlayerDead.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import * as PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { playerService } from '../../../../../infrastructure/services/PlayerService';
 import { PlayerModel } from '../../../../../infrastructure/models/PlayerModel';
 import { StoryModel } from '../../../../../infrastructure/models/StoryModel';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import styles from './Ending.module.scss';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import playerDead from '../../../../../assets/player-dead.png';
 
 class PlayerDead extends Component {
   retry = async () => {
@@ -12,18 +19,53 @@ class PlayerDead extends Component {
     window.location.reload();
   };
 
+  getTitle = () => {
+    return (
+      <Typography
+        variant="h4"
+        color="inherit"
+      >
+        You have died
+      </Typography>
+    );
+  };
+
+  getContentText = () => {
+    return (
+      <Fragment>
+        <img
+          alt="Cover"
+          src={playerDead}
+        />
+        <Typography
+          variant="h6"
+          color="inherit"
+        >
+          Your choices have led to impending doom. You lie on the ground with blurring vision thinking about what happened that led to this miserable fate.
+        </Typography>
+      </Fragment>
+    );
+  };
+
   render() {
     return (
-      <div>
-        Player is dead.
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={this.retry}
-        >
-          Try again?
-        </Button>
-      </div>
+      <Card>
+        <CardHeader title={this.getTitle()}/>
+        <CardContent>
+          {this.getContentText()}
+        </CardContent>
+        <CardActions disableActionSpacing>
+          <div className={styles.actionsContainer}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.retry}
+            >
+              Try again?
+            </Button>
+          </div>
+        </CardActions>
+      </Card>
     );
   }
 }
