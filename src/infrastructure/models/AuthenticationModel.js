1f14fc75de7dbf2bb399a92202fe61629636ac37
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-02-22 17:51:59 +0200

    Validating email before registering.

--- src/infrastructure/models/AuthenticationModel.js
+++ src/infrastructure/models/AuthenticationModel.js
@@ -35,6 +35,10 @@ export class AuthenticationModel extends BaseModel {
       delete errors.repeatPassword;
     }
 
+    if (!AuthenticationModel.validateEmail(this.email)) {
+      errors.email = 'Email is invalid';
+    }
+
     return errors;
   }
 
@@ -45,4 +49,9 @@ export class AuthenticationModel extends BaseModel {
       repeatPassword: this.repeatPassword,
     };
   }
+
+  static validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
 }
