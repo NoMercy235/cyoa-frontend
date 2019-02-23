824f89369bc3e342728bb19783bc25df8dbd8798
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-02-23 13:04:42 +0200

    The FileSelect component displays an error in a toaster if the file is too large.

--- src/shared/components/form/FileSelect/FileSelect.js
+++ src/shared/components/form/FileSelect/FileSelect.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import Button from '@material-ui/core/Button';
 import * as PropTypes from 'prop-types';
 import styles from './FileSelect.module.scss';
+import Snackbar from '../../snackbar/Snackbar';
+import { ERRORS } from '../../../constants/errors';
 
 function getBase64(file) {
   return new Promise((resolve, reject) => {
@@ -13,28 +15,47 @@ function getBase64(file) {
 }
 
 class FileSelect extends React.Component {
-  state = { file: '', base64Img: '' };
+  state = {
+    file: '',
+    base64Img: '',
+
+    // snackbar
+    open: false,
+    variant: 'success',
+    message: '',
+  };
 
   isImg = file => /^image/.test(file.type);
+
+  checkFile = file => {
+    if (file.size <= 10e5) return;
+
+    this.onChangeState({
+      open: true,
+      variant: 'error',
+      message: ERRORS.fileTooLarge,
+    })();
+    throw ERRORS.fileTooLarge;
+  };
 
   onFileUploaded = async ev => {
     const file = ev.target.files[0];
 
     if (!file) return;
-
-    if (file.size > 10e5) {
-      console.error('File larger than 1MB');
-      return;
-    }
+    this.checkFile(file);
 
     const base64 = await getBase64(file);
     this.props.onFileUploaded(base64);
 
     if (this.isImg(file)) {
-      this.setState({ base64Img: base64 });
+      this.onChangeState({ base64Img: base64 })();
     }
 
-    this.setState({ file });
+    this.onChangeState({ file })();
+  };
+
+  onChangeState = (metadata) => {
+    return () => this.setState(metadata);
   };
 
   render() {
@@ -60,6 +81,13 @@ class FileSelect extends React.Component {
           </Button>
         </label>
         <img src={this.state.base64Img} alt=""/>
+
+        <Snackbar
+          open={this.state.open}
+          onClose={this.onChangeState({ open: false })}
+          message={this.state.message}
+          variant={this.state.variant}
+        />
       </Fragment>
     );
   }
--- src/shared/constants/errors.js
+++ src/shared/constants/errors.js
@@ -1,3 +1,4 @@
 export const ERRORS = {
   fieldRequired: 'This field is required',
+  fileTooLarge: 'The file is too large',
 };
