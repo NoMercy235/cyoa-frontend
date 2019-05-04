391b5ede7a14713ace213a0e6a519553990fed6c
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-05-04 14:49:31 +0300

    Modified the manifest.json file to reflect the current app's metadata.
    Registered the service worker script and modified it so that it loads a custom made service worker which will pre-cache all static files that are in the build directory after the build script completes. I assume this service worker can also be modified to cache HTTP requests later.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,9 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // This is the custom generated service worker made by Workbox
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
