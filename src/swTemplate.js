b5405c3ba5351378379b44f8669e2dd0c6c396ca
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-05-08 22:20:57 +0300

    First caching of the tag and stories. This might change in the near future if I move the JSON responses to the IndexedDB.

--- src/swTemplate.js
+++ src/swTemplate.js
@@ -2,36 +2,69 @@
 /* eslint no-undef: 0 */
 /* eslint no-useless-escape: 0 */
 
-if ('function' === typeof importScripts) {
-  importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
-  );
-  /* global workbox */
-  if (workbox) {
-    console.log('Workbox is loaded');
-
-    /* injection point for manifest files.  */
-    workbox.precaching.precacheAndRoute([]);
-
-    /* custom cache rules*/
-    workbox.routing.registerNavigationRoute('/index.html', {
-      blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-    });
-
-    workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg)$/,
-      workbox.strategies.cacheFirst({
-        cacheName: 'images',
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 60,
-            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-          }),
-        ],
-      })
-    );
-
-  } else {
-    console.log('Workbox could not be loaded. No Offline support');
+(function () {
+  if (typeof importScripts !== 'function') {
+    console.warn('ImportScripts not available. Cannot start service worker');
+    return;
   }
+
+  importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+  );
+
+  const { workbox } = this;
+
+  if (!workbox) {
+    console.warn('Workbox could not be loaded. Aborting SW setup');
+    return;
+  }
+
+  workbox.core.setCacheNameDetails({
+    prefix: 'cyoa',
+    suffix: '1.0.0',
+  });
+
+  /* injection point for manifest files.  */
+  workbox.precaching.precacheAndRoute([]);
+
+  const matchCb = ({ url }) => {
+    return [
+      '/tag',
+      '/public/story',
+    ].includes(url.pathname);
+  };
+
+  workbox.routing.registerRoute(
+    matchCb,
+    new workbox.strategies.StaleWhileRevalidate()
+  );
+
+  configureGoogleCache (workbox);
+})();
+
+function configureGoogleCache (workbox) {
+  // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets',
+    })
+  );
+
+  // Cache the underlying font files with a cache-first strategy for 1 year.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'google-fonts-webfonts',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        }),
+      ],
+    })
+  );
 }
