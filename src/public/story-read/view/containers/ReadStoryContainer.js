e9fc0fb76a953a6f2fd73e4dab48fa0e4e49c951
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-05-26 21:26:27 +0300

    Small code refactoring for read story container.

--- src/public/story-read/view/containers/ReadStoryContainer.js
+++ src/public/story-read/view/containers/ReadStoryContainer.js
@@ -10,27 +10,36 @@ import { NOT_FOUND_ROUTE } from '../../../../shared/constants/routes';
 class ReadStoryContainer extends Component {
   state = { canRender: false, story: null, chapters: [] };
 
-  getStory = async (storyId) => {
-    try {
-      const options = { ignoreFields: 'coverPic' };
-      const story = await publicStoryService.get(storyId, options);
-      await this.getChapters(story);
-      this.setState({ story });
-    } catch (e) {
-      const { history } = this.props;
-      history.replace(NOT_FOUND_ROUTE);
-    }
+  goTo404 = () => {
+    const { history } = this.props;
+    history.replace(NOT_FOUND_ROUTE);
   };
 
-  getChapters = async () => {
-    const params = { ':story': this.props.match.params.storyId };
+  getStory = async storyId => {
+    const options = { ignoreFields: 'coverPic' };
+    const story = await publicStoryService.get(storyId, options);
+    this.setState({ story });
+  };
+
+  getChapters = async storyId => {
+    const params = { ':story': storyId };
     publicChapterService.setNextRouteParams(params);
     const chapters = await publicChapterService.list({});
-    this.setState({ chapters, canRender: true });
+    this.setState({ chapters });
   };
 
-  componentDidMount () {
-    this.getStory(this.props.match.params.storyId);
+  async componentDidMount () {
+    const storyId = this.props.match.params.storyId;
+
+    try {
+      await Promise.all([
+        this.getStory(storyId),
+        this.getChapters(storyId),
+      ]);
+    } catch (e) {
+      this.goTo404();
+    }
+    this.setState({ canRender: true });
   }
 
   render() {
