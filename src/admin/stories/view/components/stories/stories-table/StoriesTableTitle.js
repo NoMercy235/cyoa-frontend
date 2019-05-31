b3ecc474832518f6d38f9664ea80b0b718207969
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-05-31 22:19:26 +0300

    Added help text for the meaning of "offline story".
    The table container of the story container now has 100% height.

--- src/admin/stories/view/components/stories/stories-table/StoriesTableTitle.js
+++ src/admin/stories/view/components/stories/stories-table/StoriesTableTitle.js
@@ -11,6 +11,7 @@ function getDescription () {
     <>
       <p>Stories may or may not be part of a collection.</p>
       <p>You can access a story either by clicking on its name or on the view icon. From there on, you may edit multiple elements regarding the functionality of the story.</p>
+      <p>A story that is available offline will allow a user to download that story on their device and read it while not being connected to the internet. As a consequence, you will noy be able to add player attributes to that particular story and its actions will not have any consequences (since there are no attributes to modify).</p>
     </>
   );
 }
