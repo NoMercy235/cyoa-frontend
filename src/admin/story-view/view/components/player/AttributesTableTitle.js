0319d04c8de52a2049e7119f859f0819de4f8e2a
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2019-06-23 19:24:53 +0300

    chore: inform the user of the consequence of deleting an attribute.

--- src/admin/story-view/view/components/player/AttributesTableTitle.js
+++ src/admin/story-view/view/components/player/AttributesTableTitle.js
@@ -11,6 +11,7 @@ function getDescription () {
     <>
       <p>Define attributes which will be attached to a player when starting your story</p>
       <p>An important attribute must be linked to a specific ending sequence. When the value of that attribute will reach 0 or lower, the player will lose and will be shown the linked ending sequence.</p>
+      <p>Warning! Deleting an attribute will automatically remove all related consequences from the options.</p>
     </>
   );
 }
