1720032820850bf7aa30d5c90e69d32e50284d4f
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2019-08-18 00:10:25 +0300

    chore: remove unneeded logic following the backend changes (NoMercy235/cyoa-backend#1)

--- src/infrastructure/models/BaseModel.js
+++ src/infrastructure/models/BaseModel.js
@@ -3,11 +3,6 @@ export class BaseModel {
     // Error set by the server
     if (e.message) return e.message;
 
-    // Duplicate key error set by Mongo.
-    if (e.code && e.code === 11000) {
-      return `Duplicate value: ${e.errmsg.match(/"[\s\S]+"/)[0]}`;
-    }
-
-    return 'An error has occured';
+    return 'An error has occurred';
   }
 }
