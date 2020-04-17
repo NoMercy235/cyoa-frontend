8274aa42721fb95739d539840b0700910e69d07d
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2020-04-17 23:46:57 +0300

    fix: update help messages

--- src/admin/story-view/view/components/player/AttributesTableTitle.js
+++ src/admin/story-view/view/components/player/AttributesTableTitle.js
@@ -10,6 +10,7 @@ function getDescription () {
   return (
     <>
       <p>Define attributes which will be attached to a player when starting your story</p>
+      <p>Attributes can define things like stats (health, mana, etc), currencies and even items. Then, they can be used as requirements for options that lead to certain parts of the story where a player cannot go unless they have some specific items (or a specific amount of an item)</p>
       <p>An important attribute must be linked to a specific ending sequence. When the value of that attribute will reach 0 or lower, the player will lose and will be shown the linked ending sequence.</p>
       <p>Warning! Deleting an attribute will automatically remove all related consequences from the options.</p>
     </>
--- src/admin/story-view/view/components/sequences/option-table/OptionTableTitle.js
+++ src/admin/story-view/view/components/sequences/option-table/OptionTableTitle.js
@@ -14,15 +14,26 @@ function getDescription () {
         <li><b>action</b> The text that will appear to the user when they want to select this option</li>
         <li><b>leadsTo</b> The sequence that will follow after this option is chosen</li>
         <li><b>consequences</b> One or more consequences that will take effect once the option has been chosen</li>
+        <li><b>requirements</b> One or more requirements that are needed in order for that option to be displayed to the user</li>
       </ul>
       <span>The consequences have the following structure:</span>
       <ul>
         <li><b>attribute</b> The attribute that they will affect</li>
         <li><b>changeValue</b> The value which dictates how the original value of the attribute will change</li>
       </ul>
-      <p>An option is not required to have a consequence. It can be removed if you wish so.</p>
+      <span>The requirements have the following structure:</span>
+      <ul>
+        <li><b>attribute</b> The attribute that need</li>
+        <li><b>value</b> The value that needs to be fulfilled</li>
+      </ul>
+      <span>Example:</span>
+      <ul>
+        <li>An option which the following consequences: -10 Coins; +1 Sword; can be used to give a player a sword for 10 coins</li>
+        <li>The above option does, however, have a requirement of 10 Coins. If the requirement is not met, the option will not be shown to the user</li>
+      </ul>
+      <p>An option is not required to have consequences or requirements. They can be removed if you wish so.</p>
       <p>Consequences can target important attributes. Using a negative value for <b>changeValue</b> will cause that attribute to drop to negative values eventually.</p>
-      <p>Having an important attribute with negative value will immediately cause the player to lose the story.</p>
+      <p>Having an important attribute with negative value will immediately cause the player to lose the story and be shown the linked sequence of the attribute.</p>
     </>
   );
 }
